ce5b827d2802539c4b2d8894904ae340a43831bd
Sanket Nabade <sanketnabade2004@gmail.com>
2024-10-16 16:08:32 +0530

    DB connection - atlas

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB=async ()=>{
+    try {
+        // console.log(process.env.MONOGODB_URL);
+        const connectionInstance = await mongoose.connect(`${process.env.MONOGODB_URL}/${DB_NAME}`);
+        console.log(`\nDatabase connected !! MongoDB HOST : ${connectionInstance.connection.host}`)
+        // console.dir(connectionInstance.connection.host)
+    } catch (error) {
+        console.log("MongoDB connection ERROR :: ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
