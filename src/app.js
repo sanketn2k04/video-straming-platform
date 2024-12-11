057bfb82ffd93d670b78b0c391e48c06b7f5aa73
Sanket Nabade <sanketnabade2004@gmail.com>
2024-12-11 07:20:55 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express, { json } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
 
+
 const app=express();
 
 app.use(cors({
@@ -12,5 +13,21 @@ app.use(express.json({limit:'16KB'}));
 app.use(express.urlencoded({extended:true,limit:'16KB'}));
 app.use(express.static('public'));
 app.use(cookieParser());
+
+
+//routes import
+
+import userRouter from "./routes/user.route.js";
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
 
 export {app};
