cd0574c4b1ab71ba05a3cd2695503e087ca3b136
Sanket Nabade <sanketnabade2004@gmail.com>
2025-01-03 17:27:30 +0530

    Add user channel profile retrieval functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -303,7 +303,7 @@ const updateUserAvatar = asyncHandler(async(req,res,next)=>{
     return res.status(200).json(
         new ApiResponse(200, updatedUser, "Account details updated successfully!")
     );
-})
+});
 
 const updateUserCoverImage = asyncHandler(async(req,res,next)=>{
     const coverImageLocalPath=req.file.path;
@@ -326,7 +326,77 @@ const updateUserCoverImage = asyncHandler(async(req,res,next)=>{
     return res.status(200).json(
         new ApiResponse(200, updatedUser, "Account details updated successfully!")
     );
-})
+});
+
+const getUserChannelProfile = asyncHandler(async(req,res,next)=>{
+    const {username}=req.params;
+
+    if(!username?.trim()){
+        throw new ApiError(400,"Username is missing!");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+            username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers"
+            }
+        },
+        {
+            $lookup:{
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo"
+            }    
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                username:1,
+                avatar:1,
+                coverImage:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1
+            }
+        }
+        
+    ]);
+    //TODO:console.log(channel)
+
+    if(!channel?.length){
+        throw new ApiError(400,"Channel does not exists!");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, channel[0], "User Profile fetched succesfully!")
+    );
+    
+});
 
 export { 
     registerUser,
@@ -337,5 +407,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }; 
